a6929e7de90d9aa0cab3f63685553a104e341ae2
Theo <46166826+cheftheo@users.noreply.github.com>
2022-09-13 14:56:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$("#header").hide();
+	// setTimeout(function () { 
+	// 	$('#header').animate({
+	// 		opacity: 0, 
+	// 		left: '-=100'
+	// 	}, 1000);
+
+	// }, 4000);
+});
